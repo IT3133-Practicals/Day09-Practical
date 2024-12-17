cd92c588114514866a90870467a5bdb172bb141e
madurangaPrabhath <maduracourse@gmail.com>
2024-12-18 00:20:14 +0530

    Add style

--- myapp/app/(tabs)/index.tsx
+++ myapp/app/(tabs)/index.tsx
@@ -1,32 +1,38 @@
-import { Text, View,Image,StyleSheet } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      <Image source={require("../../assets/images/react-logo.png")} style={styles.logo}/>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <Image source={require("../../assets/images/react-logo.png")} style={styles.logo} />
       <Text style={styles.txt}>Hello World!</Text>
       <Text style={styles.subtxt}>My first ReactNative app</Text>
     </View>
   );
 }
-const styles= StyleSheet.create({
-  logo:{
-    width:120,
-    height:120,
+const styles = StyleSheet.create({
+  logo: {
+    width: 120,
+    height: 120,
     borderRadius: 10,
     borderWidth: 2,
     borderColor: 'lightblue',
     marginBottom: 20,
   },
-  txt:{
-    fontSize:32,
-    color:"#4B0082",
+  txt: {
+    fontSize: 32,
+    color: "#4B0082",
     fontWeight: 'bold',
     textAlign: 'center',
     marginBottom: 10,
   },
-  subtxt:{
-    fontSize: 18, 
+  subtxt: {
+    fontSize: 18,
     color: '#8B4513',
     textAlign: 'center',
   },
